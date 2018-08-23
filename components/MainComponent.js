4de1275f1d9aabcbe309c3f6fcdc75ce527aa115
Tetyana Shvets <tshvets@hotmail.com>
2018-08-23 08:20:02 -0700

    Assignment 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { View, Platform } from 'react-native';
 import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 
@@ -35,6 +37,34 @@ const HomeNavigator = createStackNavigator({
   }
 });
 
+const ContactNavigator = createStackNavigator({
+  Contact: { screen: Contact}
+}, {
+  navigationOptions: {
+    headerStyle: {
+      backgroundColor: '#512da8'
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      color: '#fff'
+    }
+  }
+});
+
+const AboutNavigator = createStackNavigator({
+  About: { screen: About}
+}, {
+  navigationOptions: {
+    headerStyle: {
+      backgroundColor: '#512da8'
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      color: '#fff'
+    }
+  }
+});
+
 const MainNavigator = createDrawerNavigator({
   Home: 
     { screen: HomeNavigator,
@@ -43,13 +73,27 @@ const MainNavigator = createDrawerNavigator({
         drawerLabel: 'Home'
       }
     },
+  About: 
+    { screen: AboutNavigator,
+      navigationOptions: {
+        title: 'About Us',
+        drawerLabel: 'About Us'
+      }
+    },
   Menu: 
     { screen: MenuNavigator,
       navigationOptions: {
         title: 'Menu',
         drawerLabel: 'Menu'
       }, 
-    }
+    },
+    Contact: 
+    { screen: ContactNavigator,
+      navigationOptions: {
+        title: 'Contact Us',
+        drawerLabel: 'Contact Us'
+      }
+    },
 }, {
     drawerBackgroundColor: '#D1C4E9'
 });
