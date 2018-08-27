ec74f010ff0cdeca79e782131c145103d6cb6e48
Tetyana Shvets <tshvets@hotmail.com>
2018-08-26 18:44:05 -0700

    Using Redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import { ScrollView, FlatList, Text } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
-import { LEADERS } from '../shared/leaders';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+      leaders: state.leaders
+    }
+}
+
+function History() {
+    return(
+        <Card title='Our History'>
+            <Text style={{margin: 10}}>
+                Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.
+            </Text> 
+            <Text style={{margin: 10}}>
+                The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.
+            </Text> 
+        </Card>
+    );
+}
 
 class About extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            leaders: LEADERS
-        }
-    }
 
     static navigationOptions = {
         title: 'About Us'
@@ -18,39 +31,33 @@ class About extends Component {
 
     render() {
         
-        const renderItem = ({item, index}) => {
+        const renderLeader = ({item, index}) => {
             return (
                 <ListItem
                     key={index}
                     title={item.name}
                     subtitle={item.description}
-                    subtitleNumberOfLines={null}
+                    NumberOfLines={15}
                     hideChevron={true}
-                    avatar={{ source: require('./images/alberto.png')}}
+                    leftAvatar={{source: {uri: baseUrl + item.image}}}
                 />
             );
         };
 
-
         return(
             <ScrollView>
-                <Card title='Our History'>
-                    <Text style={{margin: 10}}>
-                        Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.{'\n\n'}
-                        The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.
-                    </Text> 
-                </Card>
+                <History />
 
                 <Card title='Corporate Leadership'>
-                    <FlatList 
-                        data={this.state.leaders}
-                        renderItem={renderItem}
-                        keyExtractor={item => item.id.toString()}
-                    /> 
+                <FlatList 
+                    data={this.props.leaders.leaders}
+                    renderItem={renderLeader}
+                    keyExtractor={item => item.id.toString()}
+                    />
                 </Card> 
             </ScrollView>
         );
     }
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
