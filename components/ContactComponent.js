ee5c3df3421ae08f5ac3e38278113d983823baeb
Tetyana Shvets <tshvets@hotmail.com>
2018-09-01 11:42:37 -0700

    Gestures Part 1

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
 
