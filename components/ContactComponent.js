5a78bfa3141061971d98e80f697ef9df1c70db9c
Tetyana Shvets <tshvets@hotmail.com>
2018-09-01 10:20:11 -0700

    Animatable

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Text } from 'react-native';
 import { Card } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
 function Contact () {
     return(
-        <Card title='Contact Information'>
-            <Text style={{margin: 10}}>
-                121, Clear Water Bay Road{'\n\n'}
-                Clear Water Bay, Kowloon{'\n\n'}
-                HONG KONG{'\n\n'}
-                Tel: +852 1234 5678{'\n\n'}
-                Fax: +852 8765 4321{'\n\n'}
-                Email:confusion@food.net
-            </Text> 
-        </Card>
+        <ScrollView>
+            <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+                <Card title='Contact Information'>
+                    <Text style={{margin: 10}}>121, Clear Water Bay Road</Text> 
+                    <Text style={{margin: 10}}>Clear Water Bay, Kowloon</Text> 
+                    <Text style={{margin: 10}}>HONG KONG</Text> 
+                    <Text style={{margin: 10}}>Tel: +852 1234 5678</Text> 
+                    <Text style={{margin: 10}}>Fax: +852 8765 4321</Text> 
+                    <Text style={{margin: 10}}>Email:confusion@food.net</Text> 
+                </Card>
+            </Animatable.View>
+        </ScrollView>
     );
    
 }
