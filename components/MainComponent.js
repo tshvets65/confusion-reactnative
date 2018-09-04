475cfab4953ad8d973cf7cdd46f91ca1ad7e0bca
Tetyana Shvets <tshvets@hotmail.com>
2018-09-04 08:54:16 -0700

    Secure Store

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,6 +4,7 @@ import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoriteComponent';
+import Login from './LoginComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
 import { View, Platform, Image, StyleSheet, ScrollView, Text } from 'react-native';
@@ -139,6 +140,23 @@ const FavoritesNavigator = createStackNavigator({
   })
 });
 
+const LoginNavigator = createStackNavigator({
+    Login: { screen: Login }
+  }, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"            
+    },
+    headerTintColor: "#fff",
+    headerLeft: <Icon name="menu" size={24}
+      iconStyle={{ color: 'white' }} 
+      onPress={ () => navigation.toggleDrawer() } />    
+  })
+});
+
 const CustomDrawerContentComponent = (props) => (
   <ScrollView>
     <SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
@@ -156,6 +174,21 @@ const CustomDrawerContentComponent = (props) => (
 );
 
 const MainNavigator = createDrawerNavigator({
+  Login: 
+  { screen: LoginNavigator,
+    navigationOptions: {
+      title: 'Login',
+      drawerLabel: 'Login',
+      drawerIcon: ({ tintColor, focused }) => (
+        <Icon
+          name='sign-in'
+          type='font-awesome'            
+          size={24}
+          iconStyle={{ color: tintColor }}
+        />
+      ),
+    }
+  },
   Home: 
     { screen: HomeNavigator,
       navigationOptions: {
@@ -241,7 +274,8 @@ const MainNavigator = createDrawerNavigator({
       }
     }
 }, {
-    drawerBackgroundColor: '#D1C4E9',
+  initialRouteName: 'Home',  
+  drawerBackgroundColor: '#D1C4E9',
     contentComponent: CustomDrawerContentComponent
 });
 
